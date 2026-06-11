8082b518fb71f0b3c729151a5c828b517b2f92c1
Alexandre Junqueiro <tchunaartur@gmail.com>
2026-06-11 21:39:28 +0100

    get main 3

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join("public")));
 
 // Rotas
 app.get("/", (req, res) => {
-  res.sendFile(path.join("public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 /* app.listen(porta,() => {
