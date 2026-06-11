694298eb66f6028c28198d3e87c693c6fc115427
Alexandre Junqueiro <tchunaartur@gmail.com>
2026-06-11 21:56:53 +0100

    frontend 3

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 const porta = process.env.PORTA || 3000;
+const senha = process.env.SENHA;
 
 // configurações do servidor
 const app = express();
