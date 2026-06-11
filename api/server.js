26d669bf2812effa1fa6b0058c926131d36ae778
Alexandre Junqueiro <tchunaartur@gmail.com>
2026-06-11 21:19:47 +0100

    servidor

--- api/server.js
+++ api/server.js
@@ -1,17 +1,19 @@
 // Importações essenciais
 const express = require("express");
 const path = require("path");
+const { send } = require("process");
 require("dotenv").config();
 const porta = process.env.PORTA || 3000;
 
 // configurações do servidor
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join("frontend")));
+app.use(express.static(path.join("public")));
 
 // Rotas
 app.use("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res,send("mundo lindo");
+  //res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 /* app.listen(porta,() => {
