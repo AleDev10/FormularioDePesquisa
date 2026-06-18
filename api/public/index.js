c61c8263187ff34d1eafd245b5ef3bd8039d59d2
Alexandre Junqueiro <tchunaartur@gmail.com>
2026-06-18 21:47:10 +0100

    front pronto

--- api/public/index.js
+++ api/public/index.js
@@ -1,31 +1,91 @@
 //Elementos html
-const respotas = document.getElementById("resposta1");
+let respostasPtontas=[];
+let respotas = document.querySelectorAll(".entradas-info");
+respotas = [...respotas];
 const btnEnviar = document.getElementById("btn-enviar");
 
- async function enviarResposta(texto) {
+ async function enviarResposta(conteudo) {
   const resposta = await fetch("http://localhost:5000/enviar",{
     method: "POST",
     headers:{
       "content-type": "application/json"
     },
-    body: JSON.stringify({ text: texto })
+    body: JSON.stringify(conteudo)
   })
-  console.log("Resposta: ", texto);
+  console.log("enviado com sucesso");
+  console.table(conteudo);
 }
 
-function verificarTextos(texto = "") {
-  if (texto != "") {
-    enviarResposta(texto);
-  } else {
-    alert("Problemas com as respostas.");
+function filtrar_respostas(filhasRespostas){
+  if(filhasRespostas!=""){
+    var especial = /[^a-zA-Z0-9 ,.ç~`´;:\s]/.test(filhasRespostas);
+  } 
+  return especial;
+}
+
+function verificarTextos(conteudo) {
+  enviarResposta(conteudo);
+  ApagarCampos(respotas);
+}
+
+function ApagarCampos(respostas){
+  respostas.map((filhasRespostas)=>{
+    filhasRespostas.value="";
+  })
+}
+
+function verificarContiudo(){
+  let conteudoVasio=false;
+  respotas.map((conteudo)=>{
+    if(conteudo.value==""){
+      conteudoVasio=true;
+    }
+  });
+  if(conteudoVasio==true){
+    alert("Algum campo está vazio");
+  }else{
+    analizarCampos();
   }
 }
 
-function obterResposta() {
-  let texto = respotas.value;
-  verificarTextos(texto);
+function montarObjeto(conteudo) {
+  let preRespostas = {
+    respota1:conteudo[0],
+    respota2:conteudo[1],
+    respota3:conteudo[2],
+    respota4:conteudo[3],
+    respota5:conteudo[4],
+    respota6:conteudo[5],
+    respota7:conteudo[6],
+    respota8:conteudo[7],
+    respota9:conteudo[8],
+    respota10:conteudo[9],
+    respota11:conteudo[10]
+  }
+  
+  verificarTextos(preRespostas);
+}
+
+function analizarCampos() {
+  let analizeRespostas = [];
+  for (let i=0;i<respotas.length;i++){
+    analizeRespostas[i]=filtrar_respostas(respotas[i].value);
+  }
+  let resanalizeRespostas;
+ for (let i=0;i<analizeRespostas.length-1;i++){
+    resanalizeRespostas=(analizeRespostas[i]||analizeRespostas[i+1]);
+  }
+  if(resanalizeRespostas==true){
+    alert("Problemas com as respostas.");
+  }else{
+    for (let i = 0;i<respotas.length; i++) {
+      respostasPtontas[i]=respotas[i].value;
+    }
+    montarObjeto(respostasPtontas);
+    
+  }
 }
 
 btnEnviar.addEventListener("click", () => {
-  obterResposta();
+  verificarContiudo();
 });
